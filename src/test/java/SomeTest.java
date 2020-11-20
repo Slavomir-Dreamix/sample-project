b1212e06a4cc49995c0e80d05fdc3017a224b726
Slavomir <slavomir.grigorov@dreamix.eu>
2020-11-20 10:35:05 +0200

    add sample test and pom.xml

---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import javax.xml.soap.SOAPException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomeTest {
+    @Test
+    public void oneEqualsOne() throws SOAPException {
+        assertEquals(1, 1);
+    }
+
+}
